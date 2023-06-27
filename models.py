f29bce520cc8ef48c935c387836bb0409d57a53c
MihirMishra23 <mihir.mishra@gmail.com>
2023-06-27 09:50:54 -0700

    implemented email and contact classes

--- models.py
+++ models.py
@@ -7,19 +7,22 @@ Base = declarative_base()
 class Contact(Base):
   __tablename__ = 'contacts'
   id:Mapped[int] = mapped_column(primary_key=True)
-  name:Mapped[str] = mapped_column(nullable=False)
+  # name:Mapped[str] = mapped_column(nullable=False)
   email_address:Mapped[str]
-  emails: Mapped[List["Email"]] = relationship(back_populates='contact')
+  emails: Mapped[List["Email"]] = relationship(back_populates='From')
   
   def __repr__(self):
     return f"<Name = {self.name}>"
 
 class Email(Base):
   __tablename__ = 'emails'
-  id:Mapped[int] = mapped_column(primary_key=True)
-  contact_id:Mapped[int] = mapped_column(ForeignKey('contacts.id'), nullable=False)
-  text: Mapped[str] = mapped_column(Text, nullable=False)
-  contact: Mapped["Contact"] = relationship(back_populates='emails')
+  id: Mapped[int] = mapped_column(primary_key=True)
+  to_contact_id: Mapped[int] = mapped_column(ForeignKey('contacts.id'), nullable=False)
+  To: Mapped[str] = mapped_column(Text, nullable=False)
+  From: Mapped["Contact"] = relationship(back_populates='emails')
+  Date: Mapped[str] = mapped_column(Text, nullable=False)
+  Subject: Mapped[str] = mapped_column(Text, nullable=False)
+  Contents: Mapped[str] = mapped_column(Text, nullable=False)
   
   def __repr__(self):
-    return f"<Email info = {self.text}>"
+    return f"<Email info = {self.Contents}>"
