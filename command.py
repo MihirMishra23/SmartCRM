e5c58341859745ba8c7371882bed3f480ad3eea1
MihirMishra23 <48871899+MihirMishra23@users.noreply.github.com>
2025-02-10 00:54:32 -0500

    modified for postgres updates

--- command.py
+++ command.py
@@ -1,15 +1,8 @@
-# this is the drive credentails json path
-# since it should be in the same folder, just put the name of the credentials file
-DRIVE_CREDENTIALS_PATH = "drive_credentials.json"
-
-# this is the drive credentails json path
-# since it should be in the same folder, just put the name of the credentials file
-# note that if your using the same google account for both drive and gmail access,
-# this can be equal to DRIVE_CREDENTIALS_PATH
 GMAIL_CREDENTIALS_PATH = "gmail_credentials.json"
 
 # put your name here
 NAME = "Mihir Mishra"
 
-# the sheet name that you want the information to be saved to
-SHEET_NAME = "Mihir Professional Network"
+DB_NAME = "postgres"
+
+INITIALIZED = False
