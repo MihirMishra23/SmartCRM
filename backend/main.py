d7ee9fba4fb643f92ca45844840f39be8ed72fa0
MihirMishra23 <48871899+MihirMishra23@users.noreply.github.com>
2025-02-16 11:00:55 -0500

    migrated code to structured app

--- backend/main.py
+++
@@ -1,9 +0,0 @@
-from app import create_app
-from app.models.base import db
-from flask_migrate import Migrate
-
-app = create_app()
-migrate = Migrate(app, db)
-
-if __name__ == "__main__":
-    app.run(debug=True, port=5000)
