17b559013d10044e55602921ad21e872ddd060c8
MihirMishra23 <48871899+MihirMishra23@users.noreply.github.com>
2025-02-23 00:29:38 -0500

    fix(api): Enabled Flask CORS

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -2,19 +2,19 @@ from flask import Flask
 from flask_cors import CORS
 from .config import Config
 from .models.base import db
+from .api.routes import api
 
 
 def create_app(config_class=Config):
     app = Flask(__name__)
     app.config.from_object(config_class)
 
+    CORS(api, resources={r"/*": {"origins": "http://localhost:5173"}})
+
     # Initialize extensions
     db.init_app(app)
-    CORS(app)
 
     # Register blueprints
-    from .api.routes import api
-
     app.register_blueprint(api, url_prefix="/api")
 
     return app
